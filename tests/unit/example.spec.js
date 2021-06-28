83f3c6f9d62ca965b7dc35846fd7062d52b2f40d
Sumit Upadhyay <sumit.upadhyay@vmlyr.com>
2021-06-28 09:44:14 +0530

    Snapshot testing

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,11 +2,21 @@ import { shallowMount } from '@vue/test-utils'
 import Calculator from '@/components/Calculator.vue'
 
 describe('Calculator.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(Calculator, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+  //console.dir(Calculator);
+  // it('renders props.msg when passed', () => {
+  //   const msg = 'new message'
+  //   const wrapper = shallowMount(Calculator, {
+  //     propsData: { msg }
+  //   })
+  //   expect(wrapper.text()).toMatch(msg)
+  // }),
+  it('renders correctly', () => {
+    const wrapper = shallowMount(Calculator)
+    expect(wrapper.element).toMatchSnapshot()
   })
+  // test('digit & calc button clicked',() =>{
+  //   const wrapper = shallowMount(Calculator)
+  //   //wrapper.find('.digitbtn').trigger.click();
+  //   console.dir(wrapper);
+  // })
 })
